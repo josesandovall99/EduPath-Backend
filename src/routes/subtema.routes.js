51bb51aff121f17f21c3d9d89e0aa22fe3ece28a
Edgar Parada <edgaryamillparadaduarte@gmail.com>
2025-12-19 15:46:29 -0500

    Cree routes subtema

---
+++ src/routes/subtema.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const subtemaController = require('../controllers/subtema.controller');
+
+// Definición de rutas CRUD
+router.post('/', subtemaController.createSubtema);
+router.get('/', subtemaController.getSubtemas);
+router.get('/:id', subtemaController.getSubtemaById);
+router.put('/:id', subtemaController.updateSubtema);
+router.delete('/:id', subtemaController.deleteSubtema);
+
+module.exports = router;
