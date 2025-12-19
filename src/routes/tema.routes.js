67acc966dc3b26035de355d58ac760d3d17a1bf0
Edgar Parada <edgaryamillparadaduarte@gmail.com>
2025-12-19 15:38:14 -0500

    Cree routes tema

---
+++ src/routes/tema.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const temaController = require('../controllers/tema.controller');
+
+// Definición de rutas CRUD
+router.post('/', temaController.createTema);
+router.get('/', temaController.getTemas);
+router.get('/:id', temaController.getTemaById);
+router.put('/:id', temaController.updateTema);
+router.delete('/:id', temaController.deleteTema);
+
+module.exports = router;
