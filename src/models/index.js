72378aa8602f2d850df9af67c39bb41d6bee0f8a
Cristian_Estrada <cristianestrada1609@gmai.com>
2025-12-22 15:20:57 -0500

    actualizo el index.js

--- src/models/index.js
+++ src/models/index.js
@@ -4,7 +4,7 @@ const { DataTypes } = require('sequelize');
 const Miniproyecto = require('./miniproyecto.model')(sequelize, DataTypes);
 const TipoActividad = require('./tipoactividad.model')(sequelize, DataTypes);
 const actividad = require('./actividad.model')(sequelize, DataTypes);
-const evaluacion = require('./evaluacion')(sequelize, DataTypes);
+const evaluacion = require('./evaluacion.model')(sequelize, DataTypes);
 const progreso = require('./tipoactividad.model')(sequelize, DataTypes);
 
 
