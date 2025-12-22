3d801fdb1a51d8ab739ce1f4627223dc1a8c8c65
Cristian_Estrada <cristianestrada1609@gmai.com>
2025-12-22 15:10:26 -0500

    actualizo index.js con las nuevas constantes

--- src/models/index.js
+++ src/models/index.js
@@ -2,8 +2,17 @@ const sequelize = require('../config/database');
 const { DataTypes } = require('sequelize');
 
 const Miniproyecto = require('./miniproyecto.model')(sequelize, DataTypes);
+const TipoActividad = require('./tipoactividad.model')(sequelize, DataTypes);
+const actividad = require('./actividad.model')(sequelize, DataTypes);
+const evaluacion = require('./evaluacion')(sequelize, DataTypes);
+const progreso = require('./tipoactividad.model')(sequelize, DataTypes);
+
 
 module.exports = {
   sequelize,
-  Miniproyecto
+  Miniproyecto,
+  TipoActividad,
+  actividad,
+  evaluacion,
+  progreso
 };
