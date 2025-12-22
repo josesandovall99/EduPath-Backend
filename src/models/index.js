ebc9cc96d10ed7f7e8536fb168405a897cbf38b3
Edgar Parada <edgaryamillparadaduarte@gmail.com>
2025-12-22 15:04:13 -0500

    Agregue area a index

--- src/models/index.js
+++ src/models/index.js
@@ -2,8 +2,10 @@ const sequelize = require('../config/database');
 const { DataTypes } = require('sequelize');
 
 const Miniproyecto = require('./miniproyecto.model')(sequelize, DataTypes);
+const Area = require('./area.model')(sequelize, DataTypes);
 
 module.exports = {
   sequelize,
-  Miniproyecto
+  Miniproyecto,
+  Area
 };
