1a32d7204d6dcb7471cc5424a4e73094729df5b7
Edgar Parada <edgaryamillparadaduarte@gmail.com>
2026-02-19 10:53:11 -0500

    Cambio

--- src/app.js
+++ src/app.js
@@ -17,7 +17,12 @@ app.use(cors({
     credentials: true
 }));
 
-app.use(express.json());
+// Cambie esto: app.use(express.json());
+
+//Por esto:
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ limit: '50mb', extended: true }))
+
 
 // Logger para confirmar qué llega al servidor
 app.use((req, res, next) => {
